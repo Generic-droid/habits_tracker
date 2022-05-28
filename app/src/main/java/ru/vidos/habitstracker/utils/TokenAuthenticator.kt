f47877e0c81fd636c68aef43cde50e1db4249b4d
Generic-droid <58906194+Generic-droid@users.noreply.github.com>
2022-05-28 22:20:29 +0300

    Update TokenAuthenticator.kt

--- app/src/main/java/ru/vidos/habitstracker/utils/TokenAuthenticator.kt
+++ app/src/main/java/ru/vidos/habitstracker/utils/TokenAuthenticator.kt
@@ -5,7 +5,7 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
 
-const val USER_TOKEN = "8eae3a77-8547-4e12-84fd-358623e7dc39"
+const val USER_TOKEN = "Use your token"
 
 class TokenAuthenticator : Authenticator {
     override fun authenticate(route: Route?, response: Response): Request {
